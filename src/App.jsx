6441ee18aba76567aa22e2e2c2bb981a3a523e36
diegoram23 <diego.pta.oc@gmail.com>
2023-02-27 21:00:16 -0800

    completed exercise 2.6

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
-import Course from './components/Course';
+import { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', id: 1 }
+  ])
+  const [newName, setNewName] = useState('add a new name')
+
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObj = {
+      name: newName,
+      id: persons.length + 1
+    }
+    setPersons(persons.concat(nameObj))
+    setNewName('')
+
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
 
   return (
-
     <div>
-        <Course />
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        name: <input
+          value={newName}
+          onChange={handleNameChange}
+        />
+        <button type="submit">add</button>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <h5 key={person.id}>{person.name}</h5>)}
     </div>
-  );
+  )
 }
 
 export default App;
